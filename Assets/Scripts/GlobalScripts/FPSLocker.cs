03dfc41c3254b64b046eaed537912daad44eab53
DiviZition <vitya.popo0201@gmail.com>
2022-12-28 00:18:13 +0200

    Jumps was broken by different frame rate - fixed; some additional fixes;

---
+++ Assets/Scripts/GlobalScripts/FPSLocker.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81852389d5f66501f609b10cb36e3ae30e502ee3a772c28f9073f3304d423240
+size 207
--- Assets/Scripts/PlayerScripts/PlayersJumps.cs
+++ Assets/Scripts/PlayerScripts/PlayersJumps.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e173ef6ed3320f209b9e0343fab37392b00e1cfcdbb0694f291e0bc78fd2254c
-size 6457
+oid sha256:ba38c4f716feab721233883e78882e01b98d0249add7f6fe4b96c83eaccfac80
+size 6468
