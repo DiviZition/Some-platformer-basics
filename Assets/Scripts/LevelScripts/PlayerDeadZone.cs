d5ac81664f7aba817346138079373176a1f31852
DiviZition <vitya.popo0201@gmail.com>
2022-12-27 17:18:29 +0200

    DeadZone compleate;

--- Assets/Scripts/LevelScripts/PlayerDeadZone.cs
+++ Assets/Scripts/LevelScripts/PlayerDeadZone.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29efd8552c54b25e147aad423319e82149f54926fdbcd206be429b2c8c7cdd47
-size 300
+oid sha256:480c517698ca0ca88ffc8ea953f392b4f8178b01a29518a4c02d382b90d6e273
+size 1285
