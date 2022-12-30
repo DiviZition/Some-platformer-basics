5273a4ab310eeb03315be6015f7f158a53386f7a
DiviZition <vitya.popo0201@gmail.com>
2022-12-30 23:17:48 +0200

    Valid player transformation by fly platforms work in progress; Game is currently broken by this workflow, be careful !; /* TODO: almost every player transformation by moveing objects strategy in internet is faulty! I tryed a lot of stuff but it returns incorrect or invalid or way too bounded results, and I thing I have something that will work almost like I needed. That would be wayPoint that will appear for player in a contactPoint of his grounded on platform coords and parenthood. Players movement will work no more till he leave the platform, but animations will still work as usual and players movement will be retranslated to WayPoint of wich we told before. Players wigidbody will magnetically move to this way point and we will got imitation of movement on the platform;

--- Assets/Scripts/LevelScripts/FlyPlatformObjectsHolder.cs
+++ Assets/Scripts/LevelScripts/FlyPlatformObjectsHolder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:546c3c60d9d2cb97435061681fe9bb5afe09c8ae36a8b26894cb3a2437f46b01
-size 1564
+oid sha256:47e2bc5d61de75c54cbf019befc10def5ecf96d863ee05a5884b62b15e7e4f96
+size 1050
